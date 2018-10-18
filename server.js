32ab5730f6dacd14b1205b75b47716d7165e6c41
Arun Seehra <seehra.a@gmail.com>
2018-10-18 15:01:39 -0500

    Add CRUD endpoints for tags

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const { PORT } = require('./config');
 
 const notesRouter = require('./routes/notes');
 const foldersRouter = require('./routes/folders');
+const tagsRouter = require('./routes/tags');
 
 // Create an Express application
 const app = express();
@@ -29,6 +30,7 @@ app.use(express.json());
 // Mount routers on "/api"
 app.use('/api/notes', notesRouter);
 app.use('/api/folders', foldersRouter);
+app.use('/api/tags', tagsRouter);
 
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
